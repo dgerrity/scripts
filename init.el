4d2a1a82c1ec9eba7430ef988ed5ffc6d5b363af
Dan Gerrity <dan@gerrity.org>
2020-01-19 15:38:26 -0800

    adding emacs files

---
+++ init.el
@@ -0,0 +1,138 @@
+;; $Id: init.el-v 1.4 2013-06-13 16:24:27-07 dan Exp dan $
+;; $Revision: 1.4 $
+;; $Source: /Users/dan/.emacs.d/RCS/init.el-v $
+;; $Date: 2013-06-13 16:24:27-07 $
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1dc3a2e894d5ee9e90035e4ff90d57507857c07e9a394f182a961e935b3b5497" default)))
+ '(inhibit-startup-buffer-menu t)
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(setq auto-save-default nil)
+(setq 
+ backup-by-copying t
+ backup-directory-alist
+ '(("." . "~/.emacs.d/backups"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; UTF-8 encoding
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(set-language-environment "UTF-8")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; editorconfig
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'load-path "~/.emacs.d/add-ins/")
+(load "editorconfig")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Markdown mode requires a few things:
+;; Needs multimarkdown installed (sudo port install multimarkdown)
+;; Optionally install Marked.app
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load-file "~/.emacs.d/add-ins/markdown-mode.el")
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-hook 'markdown-mode-hook '(lambda() (toggle-word-wrap 1)))
+
+(defun markdown-preview-file ()
+  "run Marked on the current file and revert the buffer"
+  (interactive)
+  (shell-command 
+   (format "open -a /Applications/Marked.app %s" 
+       (shell-quote-argument (buffer-file-name))))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Use Marmalade as a code repo
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+(add-to-list 'package-archives
+    '("marmalade" .
+        "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Use aspell instead of ispell
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq ispell-program-name "/usr/local/bin/aspell")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; AppleScript mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(load-file "~/.emacs.d/add-ins/applescript.el")
+(autoload 'applescript-mode "applescript-mode"
+  "Major mode for editing AppleScript files" t)
+(add-to-list 'auto-mode-alist '("\\.applescript\\'" . applescript-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Emacs as a Cocoa app / server
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(if (featurep 'ns)
+    (progn
+      (defun ns-raise-emacs ()
+	"Raise Emacs."
+	(ns-do-applescript "tell application \"Emacs\" to activate"))
+
+      (if (display-graphic-p)
+	  (progn
+	    (add-hook 'server-visit-hook 'ns-raise-emacs)
+	    (add-hook 'before-make-frame-hook 'ns-raise-emacs)
+	    (ns-raise-emacs)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Themes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'tango t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Applescript
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun dan-as-tell-app (app something)
+  (let ((res (do-applescript (concat "tell application\""
+				     app "\" to " something))))
+    ;; the string returned is quoted
+    (substring res 1 -1)))
+
+(defun dan-as-safari-doc ()
+  (interactive)
+  (let ((url (dan-as-tell-app "Safari"
+			      "get the URL of document 1"))
+	(name (dan-as-tell-app "Safari"
+			       "get the name of window 1")))
+    (cons url name)))
+
+(defun dan-insert-safari-link ()
+  (interactive)
+  (let ((l (dan-as-safari-doc)))
+    (insert (org-make-link-string (car 1) (cdr 1)))
+    (message "Link to %s inserted" (car 1))))
+
+(setq default-frame-alist '((width . 90) (height . 54) (menu-bar-lines . 1)))
+
